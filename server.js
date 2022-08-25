13af072fd246b493b57a050f4767ca328cb5a2fa
nashalysf <nashalysf@gmail.com>
2022-08-25 19:31:37 -0400

    heroku bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 
