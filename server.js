c1f4813b8b71a4e7f2e70a229a15707d68522974
nashalysf <nashalysf@gmail.com>
2022-08-31 14:10:34 -0400

    display bug fixed, missing save and delete btns

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const app = express();
-const PORT = process.env.PORT || 3001;
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
